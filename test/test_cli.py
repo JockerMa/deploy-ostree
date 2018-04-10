eea4893f86e2c6c579a59631c00ac7f5c4cca2f0
Felix Krull <f_krull@gmx.de>
2018-04-11 00:00:19 +0200

    Return more consistent results from test util functions

--- test/test_cli.py
+++ test/test_cli.py
@@ -9,19 +9,19 @@ class TestHelp(TestCase):
     def test_should_print_error_and_exit_if_called_with_no_arguments(self):
         result = deploy_ostree([])
 
-        self.assertIn(b'the following arguments are required', result.stderr)
+        self.assertIn('the following arguments are required', result.stderr)
         self.assertEqual(2, result.returncode)
 
     def test_should_print_help_and_exit_if_called_with_help_flag(self):
         result = deploy_ostree(['--help'])
 
-        self.assertIn(b'deploy-ostree', result.stdout)
-        self.assertIn(b'deploy and configure an OSTree commit', result.stdout)
+        self.assertIn('deploy-ostree', result.stdout)
+        self.assertIn('deploy and configure an OSTree commit', result.stdout)
         self.assertEqual(0, result.returncode)
 
     def test_should_print_help_and_exit_if_called_with_h_flag(self):
         result = deploy_ostree(['-h'])
 
-        self.assertIn(b'deploy-ostree', result.stdout)
-        self.assertIn(b'deploy and configure an OSTree commit', result.stdout)
+        self.assertIn('deploy-ostree', result.stdout)
+        self.assertIn('deploy and configure an OSTree commit', result.stdout)
         self.assertEqual(0, result.returncode)
--- test/util.py
+++ test/util.py
@@ -2,9 +2,10 @@
 # Licensed under the MIT license, see LICENSE for details.
 
 import os.path
+from shutil import which
 import subprocess
 import sys
-from typing import List, Iterable, Dict, Any
+from typing import List, Iterable, Dict, Any, Optional
 
 
 SOURCE_PATH = os.path.join(os.path.dirname(__file__), os.pardir, 'src')
@@ -21,12 +22,39 @@ def get_merged_env() -> Dict[str, Any]:
     return env_with_pythonpath([source_path])
 
 
-def deploy_ostree(extra_args: List[str]) -> subprocess.CompletedProcess:
-        return subprocess.run(
-            [sys.executable, '-mdeploy_ostree'] + extra_args,
-            env=get_merged_env(),
+def maybe_decode(arg: Optional[bytes]) -> str:
+    return arg.decode(sys.getfilesystemencoding()) if arg else ''
+
+
+class ProcessResult:
+    def __init__(self, result: subprocess.CompletedProcess) -> None:
+        self.returncode = result.returncode
+        self.stdout = maybe_decode(result.stdout)
+        self.stderr = maybe_decode(result.stderr)
+
+
+def run(*args, **kwargs) -> ProcessResult:
+    return ProcessResult(subprocess.run(*args, **kwargs))
+
+
+def deploy_ostree(extra_args: List[str]) -> ProcessResult:
+    return run(
+        [sys.executable, '-mdeploy_ostree'] + extra_args,
+        env=get_merged_env(),
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+
+
+def ostree(extra_args: List[str]) -> ProcessResult:
+    try:
+        return run(
+            [which('ostree')] + extra_args,
             stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE)
+            stderr=subprocess.STDOUT,
+            check=True)
+    except subprocess.CalledProcessError as exc:
+        print(maybe_decode(exc.stdout))
+        raise
 
 
-__all__ = ['deploy_ostree']
+__all__ = ['deploy_ostree', 'ostree', 'ProcessResult']
