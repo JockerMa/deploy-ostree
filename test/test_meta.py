8ada23c7ade692568632117219498badc3b7c239
Felix Krull <f_krull@gmx.de>
2018-04-10 22:30:36 +0200

    Delete empty test

--- test/test_meta.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2018 Felix Krull
-# Licensed under the MIT license, see LICENSE for details.
-
-from unittest import TestCase
-
-
-class EmptyTest(TestCase):
-    def test(self):
-        self.assertTrue(True)
