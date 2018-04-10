19cd81dc84aa79c58e02c8b87bb2f29c4061db31
Felix Krull <f_krull@gmx.de>
2018-04-10 21:37:48 +0200

    Implement basic argument parsing

--- src/deploy_ostree/__init__.py
+++ src/deploy_ostree/__init__.py
@@ -1,9 +1,25 @@
 # Copyright 2018 Felix Krull
 # Licensed under the MIT license, see LICENSE for details.
 
+import argparse
 import sys
 
 
+def build_argument_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        prog='deploy-ostree',
+        description='deploy and configure an OSTree commit')
+
+    parser.add_argument(
+        'config',
+        metavar='CONFIG',
+        type=str,
+        nargs=1,
+        help='the path to the configuration file')
+
+    return parser
+
+
 def main():
-    print('deploy-ostree - deploy and configure an OSTree commit')
-    sys.exit(1)
+    parser = build_argument_parser()
+    parser.parse_args(sys.argv[1:])
--- test/test_cli.py
+++ test/test_cli.py
@@ -6,10 +6,22 @@ from util import deploy_ostree
 
 
 class TestHelp(TestCase):
-    def test_should_print_help_and_exit_if_called_with_no_arguments(self):
+    def test_should_print_error_and_exit_if_called_with_no_arguments(self):
         result = deploy_ostree([])
 
-        self.assertIn(
-            b'deploy-ostree - deploy and configure an OSTree commit',
-            result.stdout)
-        self.assertEqual(1, result.returncode)
+        self.assertIn(b'the following arguments are required', result.stderr)
+        self.assertEqual(2, result.returncode)
+
+    def test_should_print_help_and_exit_if_called_with_help_flag(self):
+        result = deploy_ostree(['--help'])
+
+        self.assertIn(b'deploy-ostree', result.stdout)
+        self.assertIn(b'deploy and configure an OSTree commit', result.stdout)
+        self.assertEqual(0, result.returncode)
+
+    def test_should_print_help_and_exit_if_called_with_h_flag(self):
+        result = deploy_ostree(['-h'])
+
+        self.assertIn(b'deploy-ostree', result.stdout)
+        self.assertIn(b'deploy and configure an OSTree commit', result.stdout)
+        self.assertEqual(0, result.returncode)
