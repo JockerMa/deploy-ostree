b9b2033e9eb9b3c56bd13e34a0da3ccf6e495291
Felix Krull <f_krull@gmx.de>
2018-04-11 21:32:31 +0200

    Parse URL and ref from config

--- deploy_ostree/config.py
+++ deploy_ostree/config.py
@@ -1,6 +1,23 @@
 # Copyright 2018 Felix Krull
 # Licensed under the MIT license, see LICENSE for details.
 
+import json
+from typing import TextIO
+
 
 class Config:
-    pass
+    def __init__(
+        self,
+        url: str,
+        ref: str
+    ) -> None:
+        self.url = url
+        self.ref = ref
+
+    @classmethod
+    def parse_json(cls, fobj: TextIO) -> 'Config':
+        data = json.load(fobj)
+        return Config(
+            data['ostree_url'],
+            data['ref'],
+        )
--- tests/unit/test_config.py
+++ tests/unit/test_config.py
@@ -1,11 +1,20 @@
 # Copyright 2018 Felix Krull
 # Licensed under the MIT license, see LICENSE for details.
 
+from io import StringIO
 from unittest import TestCase
-from deploy_ostree import config
+from deploy_ostree.config import Config
 
 
 class TestConfig(TestCase):
-    def test_fails(self):
-        config.Config()
-        self.fail()
+    def test_should_parse_config_with_url_and_ref(self):
+        json = '''{
+            "ostree_url": "https://example.com/ostree",
+            "ref": "fedora/28/x86_64/workstation",
+
+            "ignored key": "ignored value"
+        }'''
+
+        cfg = Config.parse_json(StringIO(json))
+        self.assertEqual('https://example.com/ostree', cfg.url)
+        self.assertEqual('fedora/28/x86_64/workstation', cfg.ref)
