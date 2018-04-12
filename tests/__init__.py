6896b4643a05b2a9e6c7ce3992d7b0c6d2d8afb3
Felix Krull <f_krull@gmx.de>
2018-04-12 20:08:39 +0200

    Use 'run' method in tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,43 +2,26 @@
 # Licensed under the MIT license, see LICENSE for details.
 
 from shutil import which
-import subprocess
 import sys
-from typing import List, Optional
-
-
-def maybe_decode(arg: Optional[bytes]) -> str:
-    return arg.decode(sys.getfilesystemencoding()) if arg else ''
-
-
-class ProcessResult:
-    def __init__(self, result: subprocess.CompletedProcess) -> None:
-        self.returncode = result.returncode
-        self.stdout = maybe_decode(result.stdout)
-        self.stderr = maybe_decode(result.stderr)
-
-
-def run(*args, **kwargs) -> ProcessResult:
-    return ProcessResult(subprocess.run(*args, **kwargs))
+from typing import List
+from deploy_ostree.run import run, ProcessResult, ProcessError
 
 
 def deploy_ostree(extra_args: List[str]) -> ProcessResult:
     return run(
         [sys.executable, '-mdeploy_ostree'] + extra_args,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE)
+        capture_output=True)
 
 
 def ostree(extra_args: List[str]) -> ProcessResult:
     try:
         return run(
             [which('ostree')] + extra_args,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
+            capture_output=True,
             check=True)
-    except subprocess.CalledProcessError as exc:
-        print(maybe_decode(exc.stdout))
+    except ProcessError as exc:
+        print(exc.process_result.stderr)
         raise
 
 
-__all__ = ['deploy_ostree', 'ostree', 'ProcessResult']
+__all__ = ['deploy_ostree', 'ostree']
--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -10,18 +10,18 @@ class TestHelp(TestCase):
         result = deploy_ostree([])
 
         self.assertIn('the following arguments are required', result.stderr)
-        self.assertEqual(2, result.returncode)
+        self.assertEqual(2, result.exitcode)
 
     def test_should_print_help_and_exit_if_called_with_help_flag(self):
         result = deploy_ostree(['--help'])
 
         self.assertIn('deploy-ostree', result.stdout)
         self.assertIn('deploy and configure an OSTree commit', result.stdout)
-        self.assertEqual(0, result.returncode)
+        self.assertEqual(0, result.exitcode)
 
     def test_should_print_help_and_exit_if_called_with_h_flag(self):
         result = deploy_ostree(['-h'])
 
         self.assertIn('deploy-ostree', result.stdout)
         self.assertIn('deploy and configure an OSTree commit', result.stdout)
-        self.assertEqual(0, result.returncode)
+        self.assertEqual(0, result.exitcode)
