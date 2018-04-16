77a4b98054c1e39fd05b78c697933b3701be3786
Felix Krull <f_krull@gmx.de>
2018-04-16 22:09:19 +0200

    Only delete (and require to delete) contents of /ostree in tests

--- tests/fixtures/ostree.py
+++ tests/fixtures/ostree.py
@@ -3,18 +3,19 @@
 
 import os
 import shutil
+import subprocess
 from .fixture import Fixture
 from .. import ostree
 
 
 class OSTreeFixture(Fixture):
     def setUp(self):
-        if os.path.exists('/ostree'):
+        if os.path.exists('/ostree') and len(os.listdir('/ostree')) > 0:
             raise Exception("these tests don't work with an existing /ostree folder")
         ostree(['admin', 'init-fs', '/'])
 
     def tearDown(self):
-        shutil.rmtree('/ostree')
+        subprocess.run('rm -rf /ostree/*', shell=True)
         shutil.rmtree('/etc/ostree/remotes.d')
         for elem in os.listdir('/boot'):
             path = os.path.join('/boot', elem)
