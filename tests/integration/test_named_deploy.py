240f80160cb86672d493455f70ad8dbd4cc3016d
Felix Krull <f_krull@gmx.de>
2018-04-16 22:16:13 +0200

    Add tests for deployments with named things

---
+++ tests/integration/test_named_deploy.py
@@ -0,0 +1,43 @@
+# Copyright 2018 Felix Krull
+# Licensed under the MIT license, see LICENSE for details.
+
+import os.path
+from unittest import skip
+from .. import deploy_ostree, ostree
+from ..fixtures import FixtureTestCase, OSTreeFixture, OSTreeCommitFixture
+
+TESTS_DIR = os.path.dirname(__file__)
+
+
+@skip('not yet implemented')
+class TestNamedDeploy(FixtureTestCase):
+    url = 'http://localhost:8000/'
+    ref = 'test-commit'
+    remote = 'test-remote'
+
+    FIXTURES = [OSTreeFixture(), OSTreeCommitFixture()]
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        deploy_ostree([os.path.join(TESTS_DIR, 'named-deploy.json')])
+
+    def test_should_add_named_remote(self):
+        url = ostree(['remote', 'show-url', self.remote]).stdout.strip()
+        self.assertEqual(
+            self.url,
+            url)
+
+    def test_should_pull_ref_from_remote(self):
+        refs = [ref.strip() for ref in ostree(['refs']).stdout.splitlines()]
+        self.assertIn('%s:%s' % (self.remote, self.ref), refs)
+
+    def test_should_create_named_stateroot(self):
+        self.assertTrue(os.path.isdir('/ostree/deploy/test-stateroot/var'))
+
+    def test_should_deploy_commit(self):
+        deployments_dir = '/ostree/deploy/test-stateroot/deploy'
+        elems = [elem for elem in os.listdir(deployments_dir) if not elem.endswith('.origin')]
+        self.assertEqual(1, len(elems))
+        deployment = os.path.join(deployments_dir, elems[0])
+        self.assertTrue(os.path.isfile(os.path.join(deployment, 'etc', 'os-release')))
