b382b2309bb7355b0200c7eb18ffd14633b45ac8
Felix Krull <f_krull@gmx.de>
2018-04-18 01:06:21 +0200

    Refactor Deploy class to be a bit more readable

--- deploy_ostree/steps/deploy.py
+++ deploy_ostree/steps/deploy.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+from typing import Set
 from . import DeployStep, DeployError
 from ..config import Config
 from ..run import run
@@ -20,29 +21,47 @@ def get_root_fs() -> str:
 class Deploy(DeployStep):
     def __init__(self, cfg: Config) -> None:
         self.cfg = cfg
-        self.ref = cfg.ref
-        self.remote = cfg.remote
-        self.stateroot = cfg.stateroot
 
     @property
     def title(self) -> str:
-        return 'Deploying %s:%s' % (self.remote, self.ref)
+        return 'Deploying %s:%s' % (self.cfg.remote, self.cfg.ref)
+
+    @property
+    def deployments_dir(self) -> str:
+        return os.path.join('/ostree', 'deploy', self.cfg.stateroot, 'deploy')
 
     def run(self):
-        deployments_dir = os.path.join('/ostree', 'deploy', self.stateroot, 'deploy')
-        elems_before_deploy = set(os.listdir(deployments_dir))
-        rootfs = get_root_fs()
+        items_diff = NewItemsHelper(self.deployments_dir)
         run([
             'ostree',
             'admin',
             'deploy',
-            '--os=%s' % self.stateroot,
-            '%s:%s' % (self.remote, self.ref),
-            '--karg=root=%s' % rootfs
+            '--os=%s' % self.cfg.stateroot,
+            '%s:%s' % (self.cfg.remote, self.cfg.ref),
+            '--karg=root=%s' % get_root_fs()
         ], check=True)
-        elems_after_deploy = set(os.listdir(deployments_dir))
-        new_elems = [elem for elem in elems_after_deploy - elems_before_deploy if not elem.endswith('.origin')]
-        if len(new_elems) != 1:
+        new_items = items_diff.get_new_items()
+
+        if len(new_items) != 1:
             raise DeployError('could not determine new deployment directory')
-        self.cfg.set_deployment_name(new_elems[0])
-        print('==> New deployment:', new_elems[0])
+        deployment_name = new_items.pop()
+
+        self.cfg.set_deployment_name(deployment_name)
+        print('==> New deployment:', deployment_name)
+
+
+class NewItemsHelper:
+    def __init__(self, path, exclude_suffix: str='.origin') -> None:
+        self.path = path
+        self.exclude_suffix = exclude_suffix
+        self.initial_items = self.get_current_items()
+
+    def get_new_items(self) -> Set[str]:
+        current_items = self.get_current_items()
+        return current_items - self.initial_items
+
+    def get_current_items(self) -> Set[str]:
+        return set(filter(self.item_is_relevant, os.listdir(self.path)))
+
+    def item_is_relevant(self, item: str) -> bool:
+        return not item.endswith(self.exclude_suffix)
