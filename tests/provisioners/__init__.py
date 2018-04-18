93b14a0cf45a73df5a88ef0dbad3159f0d8541fd
Felix Krull <f_krull@gmx.de>
2018-04-18 20:22:47 +0200

    Add /etc/fstab provisioner

---
+++ tests/provisioners/__init__.py
@@ -0,0 +1,42 @@
+# Copyright 2018 Felix Krull
+# Licensed under the MIT license, see LICENSE for details.
+
+import os
+from tempfile import TemporaryDirectory
+from typing import Dict, Iterable, Optional  # noqa
+from unittest import TestCase
+import deploy_ostree
+from deploy_ostree.run import run
+
+PROVISIONER_DIR = os.path.join(os.path.dirname(deploy_ostree.__file__), 'default-provisioners')
+
+
+class ProvisionerTestCase(TestCase):
+    PROVISIONER = None  # type: Optional[str]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.tmpdir = None
+
+    def setUp(self):
+        self.tmpdir = TemporaryDirectory()
+
+    def tearDown(self):
+        if self.tmpdir:
+            self.tmpdir.cleanup()
+
+    def path(self, *args: Iterable[str]) -> str:
+        if not self.tmpdir:
+            raise Exception('work dir not set up')
+        return os.path.join(self.tmpdir.name, *args)
+
+    def makedirs(self, *args: Iterable[str]):
+        os.makedirs(self.path(*args), mode=0o755, exist_ok=True)
+
+    def provision(self, args: Dict[str, str]={}):
+        env = {'DEPLOY_OSTREE_%s' % key: value for key, value in args.items()}
+        run(
+            [os.path.join(PROVISIONER_DIR, self.PROVISIONER), self.path()],
+            check=True,
+            env=env
+        )
---
+++ tests/provisioners/test_etc_fstab.py
@@ -0,0 +1,38 @@
+# Copyright 2018 Felix Krull
+# Licensed under the MIT license, see LICENSE for details.
+
+import os
+from . import ProvisionerTestCase
+
+
+class TestEtcFstab(ProvisionerTestCase):
+    PROVISIONER = 'etc-fstab'
+
+    def test_should_copy_fstab_from_host(self):
+        self.makedirs('etc')
+
+        self.provision()
+
+        self.should_have_copied_fstab()
+
+    def test_should_create_etc_dir_if_necessary(self):
+        self.provision()
+
+        self.assertTrue(os.path.isdir(self.path('etc')))
+        self.should_have_copied_fstab()
+
+    def test_should_replace_file_if_it_exists(self):
+        self.makedirs('etc')
+        with open(self.path('etc', 'fstab'), 'w') as f:
+            f.write('test')
+
+        self.provision()
+
+        self.should_have_copied_fstab()
+
+    def should_have_copied_fstab(self):
+        with open('/etc/fstab', 'r') as f:
+            host_fstab = f.read()
+        with open(self.path('etc', 'fstab'), 'r') as f:
+            target_fstab = f.read()
+        self.assertEqual(target_fstab, host_fstab)
