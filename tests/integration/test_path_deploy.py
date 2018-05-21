115c71fa9840b804cd59d3b5d22aa6cd7c4ed746
Felix Krull <f_krull@gmx.de>
2018-05-21 16:17:26 +0200

    Add integration test for path remote

---
+++ tests/integration/test_path_deploy.py
@@ -0,0 +1,42 @@
+# Copyright 2018 Felix Krull
+# Licensed under the MIT license, see LICENSE for details.
+
+import os.path
+import shutil
+from .. import deploy_ostree, ostree
+from ..fixtures import FixtureTestCase, OSTreeFixture, OSTreeCommitFixture
+
+TESTS_DIR = os.path.dirname(__file__)
+
+
+class TestPathDeploy(FixtureTestCase):
+    ref = 'test-commit'
+    remote = 'test-remote'
+
+    commit_fixture = OSTreeCommitFixture()
+    FIXTURES = [OSTreeFixture(), commit_fixture]
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        deploy_config = os.path.join(cls.commit_fixture.repo_dir.name, 'deploy.json')
+        shutil.copy(os.path.join(TESTS_DIR, 'path-deploy.json'), deploy_config)
+        deploy_ostree([deploy_config])
+
+    def test_should_add_named_remote(self):
+        url = ostree(['remote', 'show-url', self.remote]).stdout.strip()
+        self.assertEqual(url, 'file:%s' % self.commit_fixture.repo_dir)
+
+    def test_should_pull_ref_from_remote(self):
+        refs = [ref.strip() for ref in ostree(['refs']).stdout.splitlines()]
+        self.assertIn('%s:%s' % (self.remote, self.ref), refs)
+
+    def test_should_create_named_stateroot(self):
+        self.assertTrue(os.path.isdir('/ostree/deploy/test-stateroot/var'))
+
+    def test_should_deploy_commit(self):
+        deployments_dir = '/ostree/deploy/test-stateroot/deploy'
+        elems = [elem for elem in os.listdir(deployments_dir) if not elem.endswith('.origin')]
+        self.assertEqual(1, len(elems))
+        deployment = os.path.join(deployments_dir, elems[0])
+        self.assertTrue(os.path.isfile(os.path.join(deployment, 'etc', 'os-release')))
