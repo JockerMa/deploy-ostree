4d1fd4a7777ba4a83a7baaf807bae9ed0ff5225f
Felix Krull <f_krull@gmx.de>
2018-08-18 15:18:46 +0200

    Add integration test with existing remote

---
+++ tests/integration/test_existing_remote.py
@@ -0,0 +1,25 @@
+# Copyright 2018 Felix Krull
+# Licensed under the MIT license, see LICENSE for details.
+
+import os.path
+from .. import deploy_ostree, ostree
+from ..fixtures import FixtureTestCase, OSTreeFixture, OSTreeCommitFixture
+
+TESTS_DIR = os.path.dirname(__file__)
+
+
+class TestExistingRemote(FixtureTestCase):
+    FIXTURES = [OSTreeFixture(), OSTreeCommitFixture()]
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        ostree(['remote', 'add', 'test-remote', 'file:/some/path'])
+        deploy_ostree([os.path.join(TESTS_DIR, 'existing-remote.json')])
+
+    def test_should_replace_remote(self):
+        url = ostree(['remote', 'show-url', 'test-remote']).stdout_str.strip()
+        self.assertEqual(
+            'http://localhost:8000/',
+            url
+        )
