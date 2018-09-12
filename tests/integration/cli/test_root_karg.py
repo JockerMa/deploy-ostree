17dfbd6e806ae2f8094d52705b404297de10f28d
Felix Krull <f_krull@gmx.de>
2018-09-12 22:45:03 +0200

    Add test for default root kernel argument

--- tests/integration/cli/test_root_karg.py
+++ tests/integration/cli/test_root_karg.py
@@ -2,14 +2,34 @@
 # Licensed under the MIT license, see LICENSE for details.
 
 import os.path
+from deploy_ostree.steps.deploy import get_root_fs
 from ... import deploy_ostree
 from ...fixtures import FixtureTestCase, OSTreeFixture, OSTreeCommitFixture
 
 TESTS_DIR = os.path.join(os.path.dirname(__file__), os.pardir)
+BOOTENTRIES_DIR = os.path.join('/boot', 'loader', 'entries')
+
+
+def get_bootloader_entry():
+    entries = os.listdir(BOOTENTRIES_DIR)
+    entry_file = os.path.join(BOOTENTRIES_DIR, entries[0])
+    with open(entry_file, 'r') as f:
+        return f.read()
+
+
+class TestDefaultRootKarg(FixtureTestCase):
+    FIXTURES = [OSTreeFixture(), OSTreeCommitFixture()]
+
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        deploy_ostree([os.path.join(TESTS_DIR, 'named-deploy.json')])
+
+    def test_should_create_bootloader_entry_with_default_root_karg(self):
+        self.assertIn('root=%s' % get_root_fs(), get_bootloader_entry())
 
 
 class TestRootKarg(FixtureTestCase):
-    BOOTENTRIES_DIR = os.path.join('/boot', 'loader', 'entries')
     ROOT_KARG = '/dev/mapper/my-root-filesystem'
     FIXTURES = [OSTreeFixture(), OSTreeCommitFixture()]
 
@@ -21,13 +41,5 @@ class TestRootKarg(FixtureTestCase):
             os.path.join(TESTS_DIR, 'named-deploy.json')
         ])
 
-    def test_should_create_bootloader_entry_with_correct_root_karg(self):
-        entries = os.listdir(self.BOOTENTRIES_DIR)
-        self.assertEqual(1, len(entries))
-        entry_file = os.path.join(self.BOOTENTRIES_DIR, entries[0])
-        self.assert_file_contains(entry_file, 'root=/dev/mapper/my-root-filesystem')
-
-    def assert_file_contains(self, path, expected_content):
-        with open(path, 'r') as f:
-            content = f.read()
-        self.assertIn(expected_content, content)
+    def test_should_create_bootloader_entry_with_specified_root_karg(self):
+        self.assertIn('root=%s' % self.ROOT_KARG, get_bootloader_entry())
