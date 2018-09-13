7cd771551c87eca894ec7f08a8a939ebbab3c2c9
Felix Krull <f_krull@gmx.de>
2018-09-13 22:31:17 +0200

    Port a test to py.test

--- tests/integration/cli/test_cli.py
+++ tests/integration/cli/test_cli.py
@@ -1,27 +1,27 @@
 # Copyright 2018 Felix Krull
 # Licensed under the MIT license, see LICENSE for details.
 
-from unittest import TestCase
 from ... import deploy_ostree
 
 
-class TestHelp(TestCase):
-    def test_should_print_error_and_exit_if_called_with_no_arguments(self):
-        result = deploy_ostree([], check=False, capture_output=True)
+def should_print_error_and_exit_if_called_with_no_arguments():
+    result = deploy_ostree([], check=False, capture_output=True)
 
-        self.assertIn('the following arguments are required', result.stderr)
-        self.assertEqual(2, result.exitcode)
+    assert 'the following arguments are required' in result.stderr
+    assert result.exitcode == 2
 
-    def test_should_print_help_and_exit_if_called_with_help_flag(self):
-        result = deploy_ostree(['--help'], capture_output=True)
 
-        self.assertIn('deploy-ostree', result.stdout)
-        self.assertIn('deploy and configure an OSTree commit', result.stdout)
-        self.assertEqual(0, result.exitcode)
+def should_print_help_and_exit_if_called_with_help_flag():
+    result = deploy_ostree(['--help'], capture_output=True)
 
-    def test_should_print_help_and_exit_if_called_with_h_flag(self):
-        result = deploy_ostree(['-h'], capture_output=True)
+    assert 'deploy-ostree' in result.stdout
+    assert 'deploy and configure an OSTree commit' in result.stdout
+    assert result.exitcode == 0
 
-        self.assertIn('deploy-ostree', result.stdout)
-        self.assertIn('deploy and configure an OSTree commit', result.stdout)
-        self.assertEqual(0, result.exitcode)
+
+def should_print_help_and_exit_if_called_with_h_flag():
+    result = deploy_ostree(['-h'], capture_output=True)
+
+    assert 'deploy-ostree' in result.stdout
+    assert 'deploy and configure an OSTree commit' in result.stdout
+    assert result.exitcode == 0
