31f33aa7c3b35adec13211be8e1d0250c91aeb8d
Felix Krull <f_krull@gmx.de>
2018-09-15 01:36:22 +0200

    Add integration test for --fstab

--- tests/integration/test_fstab.py
+++ tests/integration/test_fstab.py
@@ -22,3 +22,24 @@ class TestDefaultFstab:
 
         assert fstab.exists()
         assert files_equal(fstab, Path('/etc', 'fstab'))
+
+
+@pytest.mark.usefixtures('ostree_setup', 'ostree_remote', 'deploy_ostree')
+@pytest.mark.needs_isolation
+@pytest.mark.xfail
+class TestFstabArgument:
+    deploy_ostree = ['named-deploy.json']
+
+    @pytest.fixture(scope='class', autouse=True)
+    def temp_fstab(self, tempdir_cls):
+        fstab = tempdir_cls / 'fstab'
+        with fstab.open('w') as f:
+            f.write('test fstab')
+        self.__class__.deploy_ostree.insert(0, '--fstab=%s' % fstab)
+        return fstab
+
+    def should_copy_specified_fstab_into_deployment(self, temp_fstab, ostree_setup):
+        fstab = ostree_setup.deployment('test-stateroot') / 'etc' / 'fstab'
+
+        assert fstab.exists()
+        assert files_equal(fstab, temp_fstab)
