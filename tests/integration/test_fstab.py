0dfd861fde9b569956f50180a92cf0cbcd2b8076
Felix Krull <f_krull@gmx.de>
2018-09-16 12:13:23 +0200

    Add test for fstab permissions

--- tests/integration/test_fstab.py
+++ tests/integration/test_fstab.py
@@ -2,7 +2,9 @@
 # Licensed under the MIT license, see LICENSE for details.
 
 import pytest
+import os
 from pathlib import Path
+import stat
 
 
 def files_equal(a: Path, b: Path) -> bool:
@@ -12,6 +14,14 @@ def files_equal(a: Path, b: Path) -> bool:
         return f.read() == a_content
 
 
+def assert_has_owner_and_mode(path: Path, uid: int, gid: int, mode: int) -> None:
+    st = os.stat(str(path), follow_symlinks=False)
+    assert st.st_uid == 0
+    assert st.st_gid == 0
+    assert stat.S_ISREG(st.st_mode)
+    assert stat.S_IMODE(st.st_mode) == 0o644
+
+
 @pytest.mark.usefixtures('ostree_setup', 'ostree_remote', 'deploy_ostree')
 @pytest.mark.needs_isolation
 class TestDefaultFstab:
@@ -19,9 +29,12 @@ class TestDefaultFstab:
 
     def should_copy_system_fstab_into_deployment(self, ostree_setup):
         fstab = ostree_setup.deployment('test-stateroot') / 'etc' / 'fstab'
-
         assert fstab.exists()
         assert files_equal(fstab, Path('/etc', 'fstab'))
+
+    def should_normalize_owner_and_mode(self, ostree_setup):
+        fstab = ostree_setup.deployment('test-stateroot') / 'etc' / 'fstab'
+        assert_has_owner_and_mode(fstab, 0, 0, 0o644)
 
 
 @pytest.mark.usefixtures('ostree_setup', 'ostree_remote', 'deploy_ostree')
@@ -34,11 +47,17 @@ class TestFstabArgument:
         fstab = tempdir_cls / 'fstab'
         with fstab.open('w') as f:
             f.write('test fstab')
+        os.chown(str(fstab), 1000, 1000)
+        os.chmod(str(fstab), 0o703)
         self.__class__.deploy_ostree.insert(0, '--fstab=%s' % fstab)
         return fstab
 
-    def should_copy_specified_fstab_into_deployment(self, temp_fstab, ostree_setup):
+    def should_copy_specified_fstab_into_deployment_with_correct_mode_bits(self, temp_fstab, ostree_setup):
         fstab = ostree_setup.deployment('test-stateroot') / 'etc' / 'fstab'
-
         assert fstab.exists()
         assert files_equal(fstab, temp_fstab)
+
+    @pytest.mark.xfail
+    def should_normalize_owner_and_mode(self, ostree_setup):
+        fstab = ostree_setup.deployment('test-stateroot') / 'etc' / 'fstab'
+        assert_has_owner_and_mode(fstab, 0, 0, 0o644)
